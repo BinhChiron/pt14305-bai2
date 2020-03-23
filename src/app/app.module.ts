f0f49cb24434c23f2ceb7ae679856a35402ffc6d
BinhChiron <dangthanhbinh227@gmail.com>
2020-03-24 02:57:58 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,42 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { SlideComponent } from './slide/slide.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { CategoryComponent } from './category/category.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import {FormsModule} from '@angular/forms';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { ProductUpdateComponent } from './product-update/product-update.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ProdustService } from './produst.service';
+import { AboutComponent } from './about/about.component';
 import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  RouterModule ],
-  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent, AboutComponent, ContactComponent, ProductAddComponent, ProductDetailComponent, ProductManagerComponent, ProductUpdateComponent, ServiceComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [ProdustService]
+  declarations: [
+    AppComponent,
+    SlideComponent,
+    CategoryComponent,
+    ProductListComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductManagerComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent,
+    ProductUpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
